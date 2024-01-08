4006748ac89933b5140bdf2c9823955e24c8ebc0
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-08 19:33:17 -0300

    Primeiro programa em Java

---
+++ Aulas/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,6 @@
+package Aulas.primeiroprograma;
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Java-PTBR
- Curso de Java em Curso em Vídeo.
+ Curso de Java em Curso em Vídeo. Desenvolver aplicações em Java, uma das linguagens
+
+![]()
+
+## Conteúdo
+
+Po
