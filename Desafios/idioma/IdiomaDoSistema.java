4296dc1830a8f3f066f5d9fd91c1598eebfe6aa8
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-09 12:49:03 -0300

    Desafio de resolução completo

--- Desafios/idioma/IdiomaDoSistema.java
+++ Desafios/idioma/IdiomaDoSistema.java
@@ -1,11 +1,11 @@
 package Desafios.idioma;
 
-import javax.security.auth.callback.LanguageCallback;
+import java.util.Locale;
 
 public class IdiomaDoSistema {
     public static void main(String[] args) {
-        LanguageCallback idioma = new LanguageCallback();
+        Locale pais = new Locale("Português do Brasil", "BR")
         System.out.print("Seu sistema está em ");
-        System.out.println(idioma);
+        System.out.println(pais.getLanguage());
     }
 }
--- Desafios/resolução/ResolucaoDoSistema.java
+++ Desafios/resolução/ResolucaoDoSistema.java
@@ -1,12 +1,16 @@
 package Desafios.resolução;
 
+import java.awt.Dimension;
 import java.awt.Toolkit;
 
 public class ResolucaoDoSistema {
     public static void main(String[] args) {
         Dimension res = Toolkit.getDefaultToolkit().getScreenSize();
 
+        double width = res.getWidth();
+        double height = res.getHeight();
+
         System.out.print("A resolução de sua tela é ");
-        System.out.println();
+        System.out.println((int) width + " X " + (int) height);
     }
 }
