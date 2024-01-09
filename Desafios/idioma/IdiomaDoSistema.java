72ab1dad91adc0077c4339be99a4f1843c7704e9
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-09 12:53:22 -0300

    Desafio do idioma concluído

--- Desafios/idioma/IdiomaDoSistema.java
+++ Desafios/idioma/IdiomaDoSistema.java
@@ -4,7 +4,9 @@ import java.util.Locale;
 
 public class IdiomaDoSistema {
     public static void main(String[] args) {
-        Locale pais = new Locale("Português do Brasil", "BR")
+        Locale pais = Locale.getDefault();
+
+
         System.out.print("Seu sistema está em ");
         System.out.println(pais.getLanguage());
     }
